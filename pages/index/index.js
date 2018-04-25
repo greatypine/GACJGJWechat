296866d53fce1224dbb294485290173b4a95cd26
gaolei <gaolei@guoanshequ.com>
2018-04-25 11:17:08 +0800

    gaolei test

--- pages/index/index.js
+++ pages/index/index.js
@@ -22,7 +22,7 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad  之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
